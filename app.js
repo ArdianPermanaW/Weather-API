b6a2b8c695a4d3e16c9e1f72a58cf24583ea5b99
owaladugong <ardianpermana075@gmail.com>
2025-01-06 13:05:25 +0700

    trying to use redis  it is not working atm

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
 const axios = require('axios');
 require('dotenv').config();
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+client.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
+(async () => {
+  try {
+    await client.connect();
+    console.log('Redis client connected');
+  } catch (err) {
+    console.error('Error connecting to Redis:', err);
+  }
+})();
 
 const apiKey = process.env.API_KEY;
 const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/indonesia?unitGroup=metric&key=${apiKey}&contentType=json`;
 
 let isFetching = false;
 
-async function fetchPosts() {
-  if (isFetching) return; // Prevent duplicate calls
-  isFetching = true;
-
+async function fetchPosts(url) {
   try {
-    const response = await axios.get(apiUrl);
-    console.log('Data:', response.data);
-  } catch (error) {
-    console.error('Error fetching posts:', error.message);
-  } finally {
-    isFetching = false;
+    const cachedResponse = await client.get(url);
+    if(cachedResponse){
+      console.log("cache hit!");
+      return JSON.parse(cachedResponse);
+    }
+    console.log("no cache");
+    // Fetch data from API
+    const response = await axios.get(url);
+
+    // Cache the response with a TTL of 60 seconds
+    await client.set(url, JSON.stringify(response.data), {
+      EX: 60,
+    });
+
+    return response.data;
+  }catch(err){
+    console.error('Error fetching data:', err);
+    throw err;
   }
 }
 
-fetchPosts();
+fetchPosts(apiUrl)
+.then((data) => console.log('Data:', data))
+.catch((err) => console.log(err));
 
